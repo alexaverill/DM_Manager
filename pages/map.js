ce561ab50bcec3735f396f6f390301cb77a3cfe4
Alex <alex.averill2013@gmail.com>
2020-07-04 11:21:38 -0600

    Setting up some CSS transitions.

--- pages/map.js
+++ pages/map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from '../styles/map.module.css';
 export default class Map extends React.Component{
     constructor(props){
         super(props);
@@ -10,27 +10,41 @@ export default class Map extends React.Component{
     }
     componentDidMount(){
         console.log("Mounted");
-        let _canvas = document.querySelector('canvas');
-        let _ctx = _canvas.getContext('2D');
-        this.setState({canvas:_canvas,ctx:_ctx});
+        let _canvas = document.getElementById('map-canvas');
+        let context = _canvas.getContext("2D");
+        console.log(context);
+        this.setState({canvas:_canvas,ctx:context,sidebar:false});
         this.loadImage();
     }
-    loadImage(event){
+    loadImage(){
+        console.log("Loading image");
         let image = new Image();
-        image.onLoad = () =>{
+        image.onload = () =>{
             this.state.canvas.width = image.width;
             this.state.canvas.height = image.height;
-            this.state.ctx.drawImage(image,0,0);
+            this.state.canvas.getContext('2d').drawImage(image,0,0);
+            console.log("Image loaded");
         }
+        image.onerror = ()=>{
+            console.log("Error loading image");
+        }
+        image.src = '/dnd_terrain-v1.png'
     }
     handleClick(event){
         console.log("clicked");
+        let _sidebar = this.state.sidebar;
+        this.setState({sidebar:!_sidebar});
     }
     render(){
+        let sidebarVis = this.state.sidebar ? styles.sidebar_visible : '';
+        let sidebarClasses = `${styles.sidebar} ${sidebarVis}`;
         return(
             <>
             <h1>THis is a map</h1>
-            <canvas onLoad={this.loadImage} onClick={this.handleClick}></canvas>
+            <canvas id="map-canvas" className={styles.map} onLoad={this.loadImage} onClick={this.handleClick}></canvas>
+            <div className={sidebarClasses}>
+                <h2>Data Entry!</h2>
+            </div>
             </>
         )
     }
