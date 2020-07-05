0b1543457f879a3726e4251cfc99d978fda57384
Alex <alex.averill2013@gmail.com>
2020-07-05 17:58:43 -0600

    Adding in JEST

--- models/MapPoint.js
+++ models/MapPoint.js
@@ -1,3 +1,4 @@
+//TOOD: Load from Database.
 const PointType = {
     Encounter:"Encounter",
     Treasure:"Treasure",
@@ -5,4 +6,39 @@ const PointType = {
     Cave:"Cave",
     Town:"Town",
     Shipwreck:"Shipwreck"
+}
+class MapPoint{
+    constructor(){
+        this.name = '';
+        this.type = PointType.POI;
+        this._x = 0;
+        this._y = 0;
+    }
+    get x(){
+        return this._x;
+    }
+    set x(value){
+        if(value >=0){
+            this._x = value;
+        }
+    }
+    get y(){
+        return this._y;
+    }
+    set y(value){
+        if(value>=0){
+            this._y = value;
+        }
+    }
+    get name(){
+        return this._name;
+    }
+    set name(value){
+        this._name = value;
+    }
+
+}
+module.exports = {
+    MapPoint,
+    PointType
 }
