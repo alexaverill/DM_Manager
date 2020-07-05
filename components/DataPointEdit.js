c96e22b0f2047a956855e79c014961d934cbb2f1
Alex <alex.averill2013@gmail.com>
2020-07-05 17:33:03 -0600

    Adding in some basic types.

---
+++ components/DataPointEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class DataPointEdit extends Component{
+    render(){
+        return(
+            <>
+            <form>
+                <label></label>
+            </form>
+            </>
+        )
+    }
+}
---
+++ models/MapPoint.js
@@ -0,0 +1,8 @@
+const PointType = {
+    Encounter:"Encounter",
+    Treasure:"Treasure",
+    POI:"POI",
+    Cave:"Cave",
+    Town:"Town",
+    Shipwreck:"Shipwreck"
+}
